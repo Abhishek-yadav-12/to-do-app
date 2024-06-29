77dd7e8c2e8159b84cabeabedfd28ae5877b2523
Abhishek-yadav-12 <abhishekay2003@gmail.com>
2024-06-29 14:52:56 +0530

    edited

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -5,11 +5,19 @@ const initialState = {
   const taskReducer = (state = initialState, action) => {
     switch (action.type) {
       case 'ADD_TASK':
-        return { ...state, tasks: [...state.tasks, action.task] };
+        return {...state, tasks: [...state.tasks, action.task] };
       case 'DELETE_TASK':
-        return { ...state, tasks: state.tasks.filter((task) => task.id !== action.id) };
+        return {...state, tasks: state.tasks.filter((task) => task.id!== action.taskId) };
       case 'EDIT_TASK':
-        return { ...state, tasks: state.tasks.map((task) => (task.id === action.id ? action.task : task)) };
+        return {
+         ...state,
+          tasks: state.tasks.map((task) => {
+            if (task.id === action.taskId) {
+              return {...task,...action.task };
+            }
+            return task;
+          }),
+        };
       default:
         return state;
     }
